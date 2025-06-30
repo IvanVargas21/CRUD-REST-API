1a8ad229722ce3d796d0477dfe95daac37573305
ivanvargass <165545622+IvanVargas21@users.noreply.github.com>
2025-06-30 23:00:42 +0800

    Added: "Server Initialized"

--- src/index.js
+++ src/index.js
@@ -2,5 +2,24 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
+// load environment variables from .env file into process.env
 dotenv.config();
 
+const app = express();
+const port = process.env.PORT || 5001;
+
+// Middleware
+// parse incoming reqs with JSON payloads
+app.use(express.json());
+// enable CORS for all routes
+app.use(cors());
+
+
+// Routes
+
+// Error handling middleware
+
+// Server running
+app.listen(port, () => {
+  console.log(`Server is running on port http://localhost:${port}`)
+})
